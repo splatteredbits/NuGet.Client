0306d2ce361c116dba45fc82f88ffbf242356b4e
Justin Emgarten <justin@emgarten.com>
2017-04-13 00:54:24 -0700

    Adding moq tests

--- test/NuGet.Clients.Tests/NuGet.CommandLine.Test/FeedPackagePruningTests.cs
+++ test/NuGet.Clients.Tests/NuGet.CommandLine.Test/FeedPackagePruningTests.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using NuGet.Frameworks;
-using NuGet.Packaging;
 using NuGet.Protocol;
 using NuGet.Test.Utility;
 using Xunit;
